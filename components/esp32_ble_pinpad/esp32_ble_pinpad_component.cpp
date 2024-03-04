87dfc95e20adc150cf518353032a1d9b94849b9a
Nicola Fiorillo <n.fiorillo@almaviva.it>
2024-03-04 15:55:51 +0100

    better string

--- components/esp32_ble_pinpad/esp32_ble_pinpad_component.cpp
+++ components/esp32_ble_pinpad/esp32_ble_pinpad_component.cpp
@@ -86,9 +86,6 @@ void ESP32BLEPinpadComponent::setup_characteristics() {
   this->user_id_characteristic_ = this->service_->create_characteristic(PINPAD_USERID_CHR_UUID, BLECharacteristic::PROPERTY_WRITE);
   this->user_id_characteristic_->on_write([this](const std::vector<uint8_t> &data) {
     ESP_LOGD(TAG, "Processing user message hex - %s", format_hex_pretty(data).c_str());
-    ESP_LOGD(TAG, "Processing user message - %s", data.c_str());
-
-
     this->userid_ = std::string(data.begin(), data.end());
     ESP_LOGD(TAG, "Processing user message string - %s", this->userid_);
 
