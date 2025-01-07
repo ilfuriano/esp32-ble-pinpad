7006ff4852abfcb5687a7bc90107b4aeab180d13
asm2me <49681638+asm2me@users.noreply.github.com>
2025-01-07 09:25:51 +0200

    Update esp32_ble_pinpad_component.h

--- components/esp32_ble_pinpad/esp32_ble_pinpad_component.h
+++ components/esp32_ble_pinpad/esp32_ble_pinpad_component.h
@@ -60,7 +60,7 @@ class ESP32BLEPinpadComponent : public Component, public BLEServiceComponent {
   float get_setup_priority() const override;
   void start() override;
   void stop() override;
-  std::string get_cmd() const { return this->cmd_id_; };
+
 
 
   // Setup code.
